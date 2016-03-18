fef1163651db0d787b1e09973ff6bdde6582ec54
bastien sirugue <bastien.sirugue@outlook.fr>
2016-03-18 09:46:42 +0100

    Update filAriane.php

--- application/views/system/filAriane.php
+++ application/views/system/filAriane.php
@@ -2,6 +2,7 @@
 <?php
 
 // A FAIRE PLUS TARD
+// https://openclassrooms.com/forum/sujet/generateur-automatique-de-fil-d-ariane-59766#r4637452
 
           function get_fil_ariane($array_fil) {
         $fil = '<a href="index.php">' . NOM_SITE . '</a>';
