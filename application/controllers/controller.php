c0875808e1013ab90c446c43c69f671f8781fef7
Bendawin <bastien.sirugue@outlook.fr>
2016-03-22 17:01:22 +0100

    qq

--- application/controllers/controller.php
+++ application/controllers/controller.php
@@ -32,10 +32,8 @@ class Controller extends CI_Controller {
 		
 		else
 		{
-			$this->load->helper('url');
-			$status = $this->session->userdata('status');
-			 /* $this->load->helper('url');
-			redirect ('/connecte/'); */
+		 	$this->load->helper('url');
+			redirect ('/connecte/');
 		}
 	}
 	
